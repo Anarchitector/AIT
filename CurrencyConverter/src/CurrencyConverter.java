22a0d481fe904902ba7b32e20b7beabe7b55f1cf
Anarchitector <igr.penkv@gmail.com>
2024-01-25 18:02:41 +0200

    Updated CurrencyConverter and Task006

--- CurrencyConverter/src/CurrencyConverter.java
+++ CurrencyConverter/src/CurrencyConverter.java
@@ -1,49 +1,150 @@
 import java.util.Arrays;
 import java.util.Currency;
 import java.util.Scanner;
+
 public class CurrencyConverter {
     public static void main(String[] args) {
-        int numCurr1, numCurr2, choice = 0;
+        String initCurr, finalCurr;
         double sum = 0;
         int allCurr = 3;
         Scanner sc = new Scanner(System.in);
+        String s;
         String[] currName = new String[allCurr];
-        currName[0] = "EUR";
-        currName[1] = "USD";
-        currName[2] = "MDL";
-        double[][] rates = new double[allCurr][allCurr];
-        rates[0][0] = 1;
-        rates[0][1] = 0.932400932;
-        rates[0][2] = 0.05200208;
-        rates[1][0] = 1.108893324;
-        rates[1][1] = 1;
-        rates[1][2] = 0.056721497;
-        rates[2][0] = 19.53125;
-        rates[2][1] = 17.921146953;
-        rates[2][2] = 1;
+        currName[0] = "eur";
+        currName[1] = "usd";
+        currName[2] = "mdl";
+        boolean correctCurr = false;
+
+        double eur_usd_rate = 1.07;
+        double usd_eur_rate = 0.9;
+        double eur_mdl_rate = 19.17;
+        double mdl_eur_rate = 0.05;
+        double usd_mdl_rate = 17.6;
+        double mdl_usd_rate = 0.06;
+
         System.out.print("\033[H\033[J");
         System.out.println("\n\t\t***** Приветствуем вас в программе CurrencyConverter *****\n");
-        System.out.println("Список валют:");
-        for (short i = 0; i < allCurr; i++) {
-            System.out.println((i + 1) + ". " + currName[i]);
-        }
+
         do {
-            System.out.print("Выберите валюту для продажи (Введите номер валюты): ");
-            numCurr1 = sc.nextInt();
-            if ((numCurr1 < 0) || (numCurr1 > allCurr)) {
-                System.out.println("Вы ввели неверное значение.");
+            System.out.println("Список валют:");
+            for (short i = 0; i < allCurr; i++) {
+                System.out.println((i + 1) + ". " + currName[i]);
             }
-        } while ((numCurr1 < 0) || (numCurr1 > allCurr));
-        do {
-            System.out.print(("Выберите валюту для покупки (Введите номер валюты): "));
-            numCurr2 = sc.nextInt();
-            if ((numCurr2 < 0) || (numCurr2 > allCurr)) {
-                System.out.println("Вы ввели неверное значение.");
+
+            do {
+                System.out.print("Выберите валюту для продажи (Введите номер валюты): ");
+                initCurr = sc.nextLine();
+
+                if (initCurr.equals("exit")) {
+                    break;
+                }
+                for (int i = 0; i < allCurr; i++) {
+                    if (initCurr.equals(currName[i])) {
+                        correctCurr = true;
+                        break;
+                    }
+                }
+                if (!correctCurr) {
+                    System.out.println("Вы ввели неверное значение.");
+                }
+            } while (!correctCurr);
+
+            if (initCurr.equals("exit")) {
+                return;
             }
-        } while ((numCurr2 < 0) || (numCurr2 > allCurr));
-        System.out.print("Введите сумму " + currName[numCurr1 - 1] + " для обмена на " + currName[numCurr2 - 1] + ": ");
-        sum = sc.nextDouble();
-        System.out.print("Результат: ");
-        System.out.println(sum / rates[numCurr1 - 1][numCurr2 - 1]);
+
+            correctCurr = false;
+            do {
+                System.out.print(("Выберите валюту для покупки (Введите номер валюты): "));
+                finalCurr = sc.nextLine();
+
+                if (finalCurr.equals("exit")) {
+                    break;
+                }
+                for (int i = 0; i < allCurr; i++) {
+                    if (finalCurr.equals((currName[i]))) {
+                        correctCurr = true;
+                        break;
+                    }
+                }
+                if (!correctCurr) {
+                    System.out.println("Вы ввели неверное значение.");
+                }
+            } while (!correctCurr);
+
+            if (finalCurr.equals("exit")) {
+                return;
+            }
+
+            System.out.print("Введите сумму " + initCurr + " для обмена на " + finalCurr + ": ");
+//            sum = sc.nextDouble();
+            s = sc.nextLine();
+
+            if (s.equals("exit")) {
+                return;
+            }
+            else {
+                sum = Double.parseDouble(s);
+
+                switch (initCurr) {
+                    case "eur": {
+                        switch (finalCurr) {
+                            case "usd": {
+                                sum *= eur_usd_rate;
+                                break;
+                            }
+                            case "mdl": {
+                                sum *= eur_mdl_rate;
+                                break;
+                            }
+                            default: {
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case "usd": {
+                        switch (finalCurr) {
+                            case "eur": {
+                                sum *= usd_eur_rate;
+                                break;
+                            }
+                            case "mdl": {
+                                sum *= usd_mdl_rate;
+                                break;
+                            }
+                            default: {
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    case "mdl": {
+                        switch (finalCurr) {
+                            case "eur": {
+                                sum *= mdl_eur_rate;
+                                break;
+                            }
+                            case "usd": {
+                                sum *= mdl_usd_rate;
+                                break;
+                            }
+                            default: {
+                                break;
+                            }
+                        }
+                        break;
+                    }
+                    default: {
+                        break;
+                    }
+                }
+            }
+
+
+            System.out.println("Итого: " + sum + " " + finalCurr);
+        } while ();
+
     }
+
 }
